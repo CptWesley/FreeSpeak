21a5bed33456c20de7a57cfc52960494b68b8f21
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-25 20:06:39 +0100

    Add ServerPacket

---
+++ src/FreeSpeak.Protocols.TeamSpeak.Tests/Packets/ServerPacketTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using ExtensionNet.Copy;
+using FreeSpeak.Protocols.TeamSpeak.Packets;
+using Xunit;
+using static AssertNet.Xunit.Assertions;
+
+namespace FreeSpeak.Protocols.TeamSpeak.Tests.Packets
+{
+    /// <summary>
+    /// Test class for the <see cref="ClientPacket"/> class.
+    /// </summary>
+    public class ServerPacketTests
+    {
+        private ServerPacket packet;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServerPacketTests"/> class.
+        /// </summary>
+        public ServerPacketTests()
+        {
+            packet = new ServerPacket()
+            {
+                MessageAuthenticationCode = 34,
+                PacketId = 3,
+                Flags = PacketFlags.Compressed | PacketFlags.NewProtocol,
+                Type = PacketType.CommandLow,
+                Data = new byte[] { 2, 123 }
+            };
+        }
+
+        /// <summary>
+        /// Checks that the constructor sets the correct default values.
+        /// </summary>
+        [Fact]
+        public void ConstructorTest()
+        {
+            AssertThat(packet.MessageAuthenticationCode).IsEqualTo(34);
+            AssertThat(packet.PacketId).IsEqualTo(3);
+            AssertThat(packet.Flags).IsEqualTo(PacketFlags.Compressed | PacketFlags.NewProtocol);
+            AssertThat(packet.Type).IsEqualTo(PacketType.CommandLow);
+            AssertThat(packet.Data).HasSize(2).ContainsExactly(2, 123);
+        }
+
+        /// <summary>
+        /// Checks that we can't set too large data.
+        /// </summary>
+        [Fact]
+        public void TooMuchDataTest()
+        {
+            byte[] bytes = new byte[2048];
+            AssertThat(() => packet.Data = bytes).ThrowsExactlyException<ArgumentException>()
+                .WithMessage("Maximum data length is 489, but tried to set 2048 bytes.");
+        }
+
+        /// <summary>
+        /// Checks that an object is equal to itself.
+        /// </summary>
+        [Fact]
+        public void EqualsSelfTest()
+        {
+            AssertThat(packet)
+                .IsSameAs(packet)
+                .IsEqualTo(packet)
+                .HasSameHashCodeAs(packet);
+        }
+
+        /// <summary>
+        /// Checks that an object is equal to a deep copy.
+        /// </summary>
+        [Fact]
+        public void EqualsCopyTest()
+        {
+            ServerPacket copy = packet.Copy(true);
+            AssertThat(packet)
+                .IsNotSameAs(copy)
+                .IsEqualTo(copy)
+                .HasSameHashCodeAs(copy);
+        }
+
+        /// <summary>
+        /// Checks that an object is not equal with a different mac.
+        /// </summary>
+        [Fact]
+        public void EqualsDifferentMacTest()
+        {
+            ServerPacket copy = packet.Copy(true);
+            copy.MessageAuthenticationCode = 0;
+            AssertThat(packet).IsNotEqualTo(copy);
+        }
+
+        /// <summary>
+        /// Checks that an object is not equal with a different pid.
+        /// </summary>
+        [Fact]
+        public void EqualsDifferentPidTest()
+        {
+            ServerPacket copy = packet.Copy(true);
+            copy.PacketId = 0;
+            AssertThat(packet).IsNotEqualTo(copy);
+        }
+
+        /// <summary>
+        /// Checks that an object is not equal with different flags.
+        /// </summary>
+        [Fact]
+        public void EqualsDifferentFlagsTest()
+        {
+            ServerPacket copy = packet.Copy(true);
+            copy.Flags = PacketFlags.None;
+            AssertThat(packet).IsNotEqualTo(copy);
+        }
+
+        /// <summary>
+        /// Checks that an object is not equal with a different type.
+        /// </summary>
+        [Fact]
+        public void EqualsDifferentTypeTest()
+        {
+            ServerPacket copy = packet.Copy(true);
+            copy.Type = PacketType.Pong;
+            AssertThat(packet).IsNotEqualTo(copy);
+        }
+
+        /// <summary>
+        /// Checks that an object is not equal with different data.
+        /// </summary>
+        [Fact]
+        public void EqualsDifferentDataTest()
+        {
+            ServerPacket copy = packet.Copy(true);
+            copy.Data = new byte[42];
+            AssertThat(packet).IsNotEqualTo(copy);
+        }
+
+        /// <summary>
+        /// Checks that we can convert a packet to bytes and read them as bytes again.
+        /// </summary>
+        [Fact]
+        public void ToAndFromBytesTest()
+        {
+            byte[] bytes = ServerPacket.ToBytes(packet);
+            ServerPacket copy = ServerPacket.FromBytes(bytes);
+            AssertThat(copy).IsEqualTo(packet);
+        }
+
+        /// <summary>
+        /// Checks that the correct exception is thrown when we pass invalid arguments to the function.
+        /// </summary>
+        [Fact]
+        public void ToBytesArgumentNullExceptionTest()
+            => AssertThat(() => ServerPacket.ToBytes(null)).ThrowsExactlyException<ArgumentNullException>();
+
+        /// <summary>
+        /// Checks that the correct exception is thrown when we pass invalid arguments to the function.
+        /// </summary>
+        [Fact]
+        public void FromBytesArgumentNullExceptionTest()
+            => AssertThat(() => ServerPacket.FromBytes(null)).ThrowsExactlyException<ArgumentNullException>();
+
+        /// <summary>
+        /// Checks that the correct exception is thrown when we pass invalid arguments to the function.
+        /// </summary>
+        [Fact]
+        public void FromBytesArgumentExceptionTest()
+            => AssertThat(() => ServerPacket.FromBytes(new byte[7])).ThrowsExactlyException<ArgumentException>();
+    }
+}
---
+++ src/FreeSpeak.Protocols.TeamSpeak/Packets/ServerPacket.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using ExtensionNet.Endian;
+
+namespace FreeSpeak.Protocols.TeamSpeak.Packets
+{
+    /// <summary>
+    /// Represents a packet send from the server to a client.
+    /// </summary>
+    public class ServerPacket
+    {
+        /// <summary>
+        /// The maximum data size.
+        /// </summary>
+        public const int MaxDataSize = 489;
+
+        private byte[] _data = Array.Empty<byte>();
+
+        /// <summary>
+        /// Gets or sets the EAX Message Authentication Code.
+        /// </summary>
+        public ulong MessageAuthenticationCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the packet id.
+        /// </summary>
+        public ushort PacketId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the packet flags.
+        /// </summary>
+        public PacketFlags Flags { get; set; }
+
+        /// <summary>
+        /// Gets or sets the packet type.
+        /// </summary>
+        public PacketType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        [SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "We don't mind changing the entire array.")]
+        public byte[] Data
+        {
+            get => _data;
+            set
+            {
+                if (value.Length > MaxDataSize)
+                {
+                    throw new ArgumentException($"Maximum data length is {MaxDataSize}, but tried to set {value.Length} bytes.");
+                }
+
+                _data = value;
+            }
+        }
+
+        /// <summary>
+        /// Creates a <see cref="ServerPacket"/> instance from a byte array.
+        /// </summary>
+        /// <param name="bytes">The bytes to parse.</param>
+        /// <returns>An instance parsed from the bytes.</returns>
+        public static ServerPacket FromBytes(byte[] bytes)
+        {
+            if (bytes == null)
+            {
+                throw new ArgumentNullException(nameof(bytes));
+            }
+            else if (bytes.Length < 11)
+            {
+                throw new ArgumentException($"The packet must be at least 11 bytes, but was only {bytes.Length} bytes.");
+            }
+
+            ulong mac = BitConverter.ToUInt64(bytes, 0);
+            ushort pid = BitConverter.ToUInt16(bytes, 8);
+            byte pt = bytes[10];
+            byte[] data = new byte[bytes.Length - 11];
+            Array.Copy(bytes, 11, data, 0, data.Length);
+
+            PacketFlags flags = (PacketFlags)(pt & 0xF0);
+            PacketType type = (PacketType)(pt & 0x0F);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                mac = mac.ChangeEndianness();
+                pid = pid.ChangeEndianness();
+            }
+
+            return new ServerPacket()
+            {
+                MessageAuthenticationCode = mac,
+                PacketId = pid,
+                Flags = flags,
+                Type = type,
+                Data = data
+            };
+        }
+
+        /// <summary>
+        /// Converts the object to a series of bytes.
+        /// </summary>
+        /// <param name="packet">The packet to convert.</param>
+        /// <returns>Bytes representing the packet.</returns>
+        public static byte[] ToBytes(ServerPacket packet)
+        {
+            if (packet == null)
+            {
+                throw new ArgumentNullException(nameof(packet));
+            }
+
+            return packet.ToBytes();
+        }
+
+        /// <summary>
+        /// Converts the packet to a series of bytes.
+        /// </summary>
+        /// <returns>Bytes representing the packet.</returns>
+        public byte[] ToBytes()
+        {
+            int length = Data.Length + 11;
+            byte[] result = new byte[length];
+
+            byte[] bytes;
+            bytes = BitConverter.GetBytes(BitConverter.IsLittleEndian ? MessageAuthenticationCode.ChangeEndianness() : MessageAuthenticationCode);
+            Array.Copy(bytes, 0, result, 0, bytes.Length);
+            bytes = BitConverter.GetBytes(BitConverter.IsLittleEndian ? PacketId.ChangeEndianness() : PacketId);
+            Array.Copy(bytes, 0, result, 8, bytes.Length);
+            result[10] = (byte)((byte)Flags + (byte)Type);
+            Array.Copy(Data, 0, result, 11, Data.Length);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is ServerPacket other)
+            {
+                return MessageAuthenticationCode == other.MessageAuthenticationCode
+                    && PacketId == other.PacketId
+                    && Flags == other.Flags
+                    && Type == other.Type
+                    && _data.SequenceEqual(other._data);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            int mac1 = (int)(MessageAuthenticationCode & 0x00000000FFFFFFFF);
+            int mac2 = (int)(MessageAuthenticationCode & 0xFFFFFFFF00000000);
+            int mac = mac1 + mac2;
+
+            return GetType().GetHashCode() * (mac + (2 * PacketId) + (3 * ((int)Flags + (int)Type)) + (4 * _data.Sum(x => x)));
+        }
+    }
+}
