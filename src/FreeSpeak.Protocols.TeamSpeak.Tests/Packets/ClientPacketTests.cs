50178b8191e1f6b0c8aa1cf2556599546ec127ba
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-25 14:46:31 +0100

    Add ClientPacket class

---
+++ src/FreeSpeak.Protocols.TeamSpeak.Tests/Packets/ClientPacketTests.cs
@@ -0,0 +1,35 @@
+﻿using FreeSpeak.Protocols.TeamSpeak.Packets;
+using Xunit;
+
+using static AssertNet.Xunit.Assertions;
+
+namespace FreeSpeak.Protocols.TeamSpeak.Tests.Packets
+{
+    /// <summary>
+    /// Test class for the <see cref="ClientPacket"/> class.
+    /// </summary>
+    public static class ClientPacketTests
+    {
+        /// <summary>
+        /// Checks that the constructor sets the correct default values.
+        /// </summary>
+        [Fact]
+        public static void ConstructorTest()
+        {
+            ClientPacket packet = new ClientPacket()
+            {
+                MessageAuthenticationCode = 3425,
+                PacketId = 2,
+                ClientId = 435,
+                PacketType = 12,
+                Data = new byte[] { 33, 45, 72 }
+            };
+
+            AssertThat(packet.MessageAuthenticationCode).IsEqualTo(3425);
+            AssertThat(packet.PacketId).IsEqualTo(2);
+            AssertThat(packet.ClientId).IsEqualTo(435);
+            AssertThat(packet.PacketType).IsEqualTo(12);
+            AssertThat(packet.Data).HasSize(3).ContainsExactly(33, 45, 72);
+        }
+    }
+}
---
+++ src/FreeSpeak.Protocols.TeamSpeak/Packets/ClientPacket.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FreeSpeak.Protocols.TeamSpeak.Packets
+{
+    /// <summary>
+    /// Represents a packet send from the client to the server.
+    /// </summary>
+    public class ClientPacket
+    {
+        /// <summary>
+        /// The maximum data size.
+        /// </summary>
+        public const int MaxDataSize = 487;
+
+        private byte[] _data = Array.Empty<byte>();
+
+        /// <summary>
+        /// Gets or sets the EAX Message Authentication Code.
+        /// </summary>
+        public ulong MessageAuthenticationCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the packet id.
+        /// </summary>
+        public ushort PacketId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the client id.
+        /// </summary>
+        public ushort ClientId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the packet type.
+        /// </summary>
+        public byte PacketType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        [SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "We don't mind changing the entire array.")]
+        public byte[] Data
+        {
+            get => _data;
+            set
+            {
+                if (value.Length > MaxDataSize)
+                {
+                    throw new ArgumentException($"Maximum data length is {MaxDataSize}, but tried to set {value.Length} bytes.");
+                }
+
+                _data = value;
+            }
+        }
+    }
+}
