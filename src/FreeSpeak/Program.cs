2e7ca05b88f4ce4007fc8f040456a7b563c417df
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-25 14:19:15 +0100

    Add basic project

---
+++ src/FreeSpeak/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FreeSpeak
+{
+    /// <summary>
+    /// Entry point of the program.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The program arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
