a123f1f3f044550b949245f9f548dbf6de26b4e3
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-26 14:46:42 +0100

    Add a console logger class

---
+++ src/FreeSpeak/ConsoleLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FreeSpeak.Servers;
+
+namespace FreeSpeak
+{
+    /// <summary>
+    /// Class logging things to the console.
+    /// </summary>
+    /// <seealso cref="ILogger" />
+    public class ConsoleLogger : ILogger
+    {
+        /// <inheritdoc/>
+        public void WriteError(string message)
+            => WriteLine($"[ERROR] {message}", ConsoleColor.Red);
+
+        /// <inheritdoc/>
+        public void WriteInfo(string message)
+            => WriteLine($"[INFO] {message}", ConsoleColor.White);
+
+        /// <inheritdoc/>
+        public void WriteWarning(string message)
+            => WriteLine($"[WARNING] {message}", ConsoleColor.Yellow);
+
+        private void WriteLine(string text, ConsoleColor color)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}
