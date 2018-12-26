47ebde9972fc9d3f83666604254b717ffe044ae9
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-26 14:00:02 +0100

    Add FreeSpeak.Servers and FreeSpeak.Servers.TeamSpeak projects

---
+++ src/FreeSpeak.Servers/IServer.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeSpeak.Servers
+{
+    /// <summary>
+    /// Interface for FreeSpeak servers.
+    /// </summary>
+    public interface IServer
+    {
+        /// <summary>
+        /// Gets or sets the logger used for logging messages.
+        /// </summary>
+        ILogger Logger { get; set; }
+    }
+}
