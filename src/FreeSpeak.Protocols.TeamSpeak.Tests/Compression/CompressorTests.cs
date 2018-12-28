4b42fd8f5b0dc26d6c73c10a7f44b591e0540610
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-28 15:26:22 +0100

    Add compressor class

---
+++ src/FreeSpeak.Protocols.TeamSpeak.Tests/Compression/CompressorTests.cs
@@ -0,0 +1,22 @@
+﻿using FreeSpeak.Protocols.TeamSpeak.Compression;
+using Xunit;
+using static AssertNet.Xunit.Assertions;
+
+namespace FreeSpeak.Protocols.TeamSpeak.Tests.Compression
+{
+    /// <summary>
+    /// Test class for the <see cref="Compressor"/> class.
+    /// </summary>
+    public class CompressorTests
+    {
+        private Compressor compressor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompressorTests"/> class.
+        /// </summary>
+        public CompressorTests()
+        {
+            compressor = new Compressor();
+        }
+    }
+}
---
+++ src/FreeSpeak.Protocols.TeamSpeak/Compression/Compressor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FreeSpeak.Protocols.TeamSpeak.Compression
+{
+    /// <summary>
+    /// Class used for compressing data.
+    /// </summary>
+    public class Compressor
+    {
+        /// <summary>
+        /// Compresses the specified data.
+        /// </summary>
+        /// <param name="data">The data to compress.</param>
+        /// <returns>The compressed data.</returns>
+        public byte[] Compress(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
